60a167359fc1f63d03e89ce910d0b5017815b081
Huang.Weijie <wjhuang@live.cn>
2017-01-23 17:32:07 +0800

    finished

---
+++ src/test/java/indi/weijie/vertx/MyFirstVerticleTest.java
@@ -0,0 +1,46 @@
+package indi.weijie.vertx;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by GHuang on 2017/1/23.
+ */
+@RunWith(VertxUnitRunner.class)
+public class MyFirstVerticleTest {
+
+    private Vertx vertx;
+
+    @Before
+    public void setUp(TestContext context) throws Exception {
+        vertx = Vertx.vertx();
+        vertx.deployVerticle(MyFirstVerticle.class.getName(),
+                context.asyncAssertSuccess());
+    }
+
+    @After
+    public void tearDown(TestContext context) throws Exception {
+        vertx.close(context.asyncAssertSuccess());
+    }
+
+    @Test
+    public void testMyApplication(TestContext context) throws Exception {
+        final Async async = context.async();
+
+        vertx.createHttpClient().getNow(8080, "localhost", "/",
+                response -> {
+                    response.handler(body -> {
+                        context.assertTrue(body.toString().contains("Hello"));
+                        async.complete();
+                    });
+                });
+    }
+}
