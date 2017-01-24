d6c3a87776a6f1a655208363aaf7fc282a7ccc9c
Huang.Weijie <wjhuang@live.cn>
2017-01-24 16:41:04 +0800

    post-4

--- src/test/java/indi/weijie/vertx/MyFirstVerticleTest.java
+++ src/test/java/indi/weijie/vertx/MyFirstVerticleTest.java
@@ -2,6 +2,7 @@ package indi.weijie.vertx;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
@@ -10,6 +11,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.net.ServerSocket;
 
 import static org.junit.Assert.*;
 
@@ -25,12 +28,13 @@ public class MyFirstVerticleTest {
     @Before
     public void setUp(TestContext context) throws Exception {
         vertx = Vertx.vertx();
-
+        ServerSocket socket = new ServerSocket(0);
+        port = socket.getLocalPort();
+        socket.close();
         DeploymentOptions options = new DeploymentOptions()
-                .setConfig(new JsonObject().put("http.port", port));
-
-        vertx.deployVerticle(MyFirstVerticle.class.getName(), options,
-                context.asyncAssertSuccess());
+                .setConfig(new JsonObject().put("http.port", port)
+                );
+        vertx.deployVerticle(MyFirstVerticle.class.getName(), options, context.asyncAssertSuccess());
     }
 
     @After
@@ -49,5 +53,41 @@ public class MyFirstVerticleTest {
                         async.complete();
                     });
                 });
+    }
+
+    @Test
+    public void checkThatTheIndexPageIsServed(TestContext context) {
+        Async async = context.async();
+        vertx.createHttpClient().getNow(port, "localhost", "/assets/index.html", response -> {
+            context.assertEquals(response.statusCode(), 200);
+            context.assertEquals(response.headers().get("content-type"), "text/html;charset=UTF-8");
+            response.bodyHandler(body -> {
+                context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
+                async.complete();
+            });
+        });
+    }
+
+    @Test
+    public void checkThatWeCanAdd(TestContext context) {
+        Async async = context.async();
+        final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+        final String length = Integer.toString(json.length());
+        vertx.createHttpClient().post(port, "localhost", "/api/whiskies")
+                .putHeader("content-type", "application/json")
+                .putHeader("content-length", length)
+                .handler(response -> {
+                    context.assertEquals(response.statusCode(), 201);
+                    context.assertTrue(response.headers().get("content-type").contains("application/json"));
+                    response.bodyHandler(body -> {
+                        final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+                        context.assertEquals(whisky.getName(), "Jameson");
+                        context.assertEquals(whisky.getOrigin(), "Ireland");
+                        context.assertNotNull(whisky.getId());
+                        async.complete();
+                    });
+                })
+                .write(json)
+                .end();
     }
 }
